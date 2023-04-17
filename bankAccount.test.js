086ae6479e34f4e818992d831c8b1aef55abec32
Valeria Serci <balerty84@gmail.com>
2023-04-17 17:42:44 +0100

    test drive deposit method

--- bankAccount.test.js
+++ bankAccount.test.js
@@ -1,8 +1,20 @@
 const BankAccount = require('./bankAccount');
 
 describe('BankAccount', () => {
-  it('has an initial balance of 0', () => {
+
+  //beforeEach(() => {
+  //});
+  test('has an initial balance of 0', () => {
     const bankAccount = new BankAccount();
     expect(bankAccount.getBalance()).toEqual(0);
+  });
+
+  test('has a balance of 100 with a deposit of 100', () => {
+    const bankAccount = new BankAccount();
+    bankAccount.deposit(100);
+    expect(bankAccount.getBalance()).toEqual(100);
   })
+
+
+
 })
