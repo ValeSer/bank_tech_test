ab3a275158ea7d64647d3df8e644db12c47295e6
Valeria Serci <balerty84@gmail.com>
2023-04-19 16:14:31 +0100

    refactor printBankStatement and create two separate private functions

--- bankAccount.js
+++ bankAccount.js
@@ -25,18 +25,25 @@ class BankAccount {
   }
 
   printBankStatement() {
+    this._logHeader();
+    this.transactions.forEach(transaction => {
+      this._logTransaction(transaction);
+    });
+  }
+
+  _logHeader() {
     console.log('  date'.padEnd(11) + '||' +
       '  credit'.padEnd(11) + '||' +
       '  debit'.padEnd(11) + '||' +
       '  balance'.padEnd(11));
+  }
 
-    this.transactions.forEach(transaction => {
-      console.log(
-        moment(transaction.date).format('DD/MM/YYYY') + ' || ' +
-        (transaction.type === 'deposit' ? transaction.amount.toFixed(2) : '').padStart(9) + ' || ' +
-        (transaction.type === 'withdrawal' ? transaction.amount.toFixed(2) : '').padStart(9) + ' || ' +
-        transaction.balance.toFixed(2).padStart(9) + ' ');
-    });
+  _logTransaction(transaction) {
+    console.log(
+      moment(transaction.date).format('DD/MM/YYYY') + ' || ' +
+      (transaction.type === 'deposit' ? transaction.amount.toFixed(2) : '').padStart(9) + ' || ' +
+      (transaction.type === 'withdrawal' ? transaction.amount.toFixed(2) : '').padStart(9) + ' || ' +
+      transaction.balance.toFixed(2).padStart(9) + ' ');
   }
 }
 
